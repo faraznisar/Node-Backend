a83a7e651d23839d6ca0c055451c89b746b3f9a7
Shah <fshah7@dxc.com>
2020-02-26 10:12:36 +0530

    Added error model

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,27 +1,52 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error')
+
 const router = express.Router();
+
 const DUMMY_PLACES = [
     {
         id: 'p1',
         title: 'Empire State Building',
         description: 'One of the most famous sky scrapers in the world!',
-        imageUrl:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-        address: '20 W 34th St, New York, NY 10001',
         location: {
-            lat: 40.7484405,
-            lng: -73.9878584
+            lat: 40.7484474,
+            lng: -73.9871516
         },
+        address: '20 W 34th St, New York, NY 10001',
         creator: 'u1'
-    }]
+    }
+];
 
 router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid;
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId
     })
+    if (!place) {
+
+        throw new HttpError('Could not find a place with the provided Id', 404)
+    }
     res.json({ place })
 })
+// router.get('/user/:uid', (req, res, next) => {
+//     const userId = req.params.uid;
+//     const place = DUMMY_PLACES.find(p => {
+//         return p.creator === userId
+//     })
+//     res.json({ place })
+// })
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId
+    })
+    if (!place) {
+        return next(new HttpError('Could not find a place with the provided user Id'))
+    }
+    res.json({ place })
+})
+
 
 module.exports = router
