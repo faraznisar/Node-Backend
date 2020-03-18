16ce999bcac05d9cfda22c12a0fe748327dbac81
Shah <fshah7@dxc.com>
2020-03-18 12:22:22 +0530

    Added mimetype/ multer for file uploads

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,17 +2,19 @@ const express = require('express')
 const { check } = require('express-validator')
 
 const userControllers = require('../controllers/users-controllers')
+const fileUpload = require('../middleware/file-upload')
 
 const router = express.Router()
 
 router.get('/', userControllers.getUsers)
 
 router.post('/signup',
-    [
-        check('name').not().isEmpty(),
-        check('email').normalizeEmail().isEmail(),
-        check('password').isLength({ min: 6 })
-    ],
+    fileUpload.single('image');
+[
+    check('name').not().isEmpty(),
+    check('email').normalizeEmail().isEmail(),
+    check('password').isLength({ min: 6 })
+],
     userControllers.signup)
 router.post('/login', userControllers.login)
 
