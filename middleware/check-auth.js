2d807fe742ae2399af94b644d9482fda38bce230
Shah <fshah7@dxc.com>
2020-04-05 16:33:48 +0530

    Added bcrypt for password encryption and starting JWT for tokens

---
+++ middleware/check-auth.js
@@ -0,0 +1,19 @@
+const HttpError = require('../models/http-error')
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+
+        if (!token) {
+            throw new Error('Authorization failed!')
+        }
+        const decodedToken = jwt.verify(token, 'privatekey_never_share')
+        req.userData = { userId: decodedToken.userId }
+        next();
+    } catch (err) {
+        const error = new HttpError('Authorization failed!', 401)
+        return next(error)
+    }
+
+}
